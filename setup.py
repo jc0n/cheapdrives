e18384cf129f77d3f65c27f39ac646899e40a534
John O'Connor <tehjcon@gmail.com>
2011-10-07 00:41:36 -0400

    Release cheapdrives on github

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cheap Drives',
+      version='0.2',
+      description='Finds cheap hard drives',
+      author='John O\'Connor',
+      author_email='tehjcon@gmail.com',
+      scripts=['cheapdrives']
+      )
